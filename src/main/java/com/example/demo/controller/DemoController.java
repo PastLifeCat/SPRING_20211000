6054bfdcc134b1bb84192acf29afcb3148fd6336
tkdltk9 <tkdltk9@naver.com>
2024-10-16 11:20:13 +0900

    20211000_1016수업

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,19 +1,29 @@
-package com.example.demo;
+package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController {
+
+    @Autowired // 객체 주입 자동화
+    private TestService testService;
+
     @GetMapping("/hello") // 전송 방식 GET
         public String hello(Model model) {
             model.addAttribute("data", " 방갑습니다."); // model 설정
         return "hello"; // hello.html 연결
         }
+
     @GetMapping("/about_detailed")
         public String about_detailed() {
         return "about_detailed";
     }
+
     @GetMapping("/test1")
         public String thymeleaf_test1(Model model) {
             model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
@@ -24,4 +34,12 @@ public class DemoController {
             model.addAttribute("para2", 002);
         return "thymeleaf_test1";
         }
+
+    @GetMapping("/testdb")
+        public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        model.addAttribute("data4", test);
+        System.out.println("데이터 출력 디버그 : " + test);
+        return "testdb";
+    }
 } 
