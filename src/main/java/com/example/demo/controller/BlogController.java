63b8d35751eecc40486e9cb200e7cef51d153415
PastLifeCat <yjj2486123@gmail.com>
2024-12-15 00:03:42 +0900

    자바웹프_진짜최종종

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -24,60 +24,11 @@ public class BlogController {
     @Autowired // 객체 주입 자동화
     BlogService blogService; // DemoController 클래스 아래 객체 생성
 
-    // @GetMapping("/article_list") // 게시판 링크 지정
-    // public String article_list(Model model) {
-    //     List<Article> list = blogService.findAll(); // 게시판 리스트
-    //     model.addAttribute("articles", list); // 모델에 추가
-    //     return "article_list"; // .HTML 연결
-    // }
-
-    // @GetMapping("/article_edit/{id}") // 게시판 링크 지정
-    // public String article_edit(Model model, @PathVariable Long id) {
-    //     Optional<Article> list = blogService.findById(id); // 선택한 게시판 글
-    //     if (list.isPresent()) {
-    //         model.addAttribute("article", list.get()); // 존재하면 Article 객체를 모델에 추가
-    //     } else {
-    //         // 처리할 로직 추가 (예: 오류 페이지로 리다이렉트, 예외 처리 등)
-    //         return "/error_page/article_error"; // 오류 처리 페이지로 연결(이름 수정됨)
-    //     }
-    //     return "article_edit"; // .HTML 연결
-    // }
-
-    // @PutMapping("/api/article_edit/{id}")
-    // public String updateArticle(@PathVariable Long id, @ModelAttribute AddArticleRequest request) {
-    //     blogService.update(id, request);
-    //     return "redirect:/article_list"; // 글 수정 이후 .html 연결
-    // }
-
     @DeleteMapping("/api/article_delete/{id}")
     public String deleteArticle(@PathVariable Long id) {
         blogService.delete(id);
         return "redirect:/article_list";
     }
-
-    // @GetMapping("/board_list") // 새로운 게시판 링크 지정
-    // public String board_list(Model model) {
-    //     List<Board> list = blogService.findAll(); // 게시판 전체 리스트
-    //     model.addAttribute("boards", list); // 모델에 추가
-    //     return "board_list"; // .HTML 연결
-    // }
-
-    // @GetMapping("/board_list") // 새로운 게시판 링크 지정
-    // public String board_list(Model model, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "") String keyword) {
-    //     PageRequest pageable = PageRequest.of(page, 3); // 한 페이지의 게시글 수
-    //     Page<Board> list; // Page를 반환
-        
-    //     if (keyword.isEmpty()) {
-    //         list = blogService.findAll(pageable); // 기본 전체 출력(키워드 x)
-    //     } else {
-    //         list = blogService.searchByKeyword(keyword, pageable); // 키워드로 검색
-    //     }
-    //     model.addAttribute("boards", list); // 모델에 추가
-    //     model.addAttribute("totalPages", list.getTotalPages()); // 페이지 크기
-    //     model.addAttribute("currentPage", page); // 페이지 번호
-    //     model.addAttribute("keyword", keyword); // 키워드
-    //     return "board_list"; // .HTML 연결
-    // }
 
     @GetMapping("/board_list") // 새로운 게시판 링크 지정
     public String board_list(
--- src/main/java/com/example/demo/controller/BlogRestController.java
+++ src/main/java/com/example/demo/controller/BlogRestController.java
@@ -1,20 +1,11 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController // @Controller + @ResponseBody
 public class BlogRestController {
-    private final BlogService blogService;
-
-    // @PostMapping("/api/articles")
-    // public ResponseEntity<Article> addArticle(@ModelAttribute AddArticleRequest request) {
-    //     Article saveArticle = blogService.save(request);
-    //     return ResponseEntity.status(HttpStatus.CREATED)
-    //     .body(saveArticle);
-    // }
 
     @GetMapping("/favicon.ico")
     public void favicon() {
